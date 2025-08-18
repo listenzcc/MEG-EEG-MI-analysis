176c4b9b0645ebd3f9c62a750dbd4213f13eb2f0
listenzcc <chuncheng.zhang@ia.ac.cn>
2025-08-18 11:08:18 +0800

    Add plotting function to the FBCSP.vote.summary.

--- mvpa.4.FBCSP.vote.summary.py
+++ mvpa.4.FBCSP.vote.summary.py
@@ -7,79 +7,107 @@ from util.io.file import load
 
 # %%
 # Summary the CSP
-data_directory = Path('./data/MVPA.FBCSP.vote.meg.fine')
 
-data_files = list(data_directory.rglob('*.dump'))
-data_files.sort()
-print(data_files)
+output_directory = Path('./data/MVPA.FBCSP.vote.results')
+output_directory.mkdir(parents=True, exist_ok=True)
 
+data_directory = Path('./data/MVPA.FBCSP.vote.eeg.fine')
 
-def vote(preds):
-    candidates = {k: 0 for k in [1, 2, 3, 4, 5]}
-    for i, e in enumerate(preds):
-        candidates[e] += 1
-    return sorted(candidates.items(), key=lambda e: e[1])
+data_directories = [
+    Path('./data/MVPA.FBCSP.vote.eeg.fine'),
+    Path('./data/MVPA.FBCSP.vote.eeg'),
+    Path('./data/MVPA.FBCSP.vote.meg.fine'),
+    Path('./data/MVPA.FBCSP.vote.meg')
+]
 
+for data_directory in data_directories:
+    name = data_directory.name
+    print(f'Working with {name=}')
 
-data = []
-yy_true = []
-yy_pred = []
-yy_pred_2 = []
-y_probas_stack = []
-y_true_stack = []
-for f in data_files:
-    d = load(f)
-    subject = d['subject_name']
+    data_files = list(data_directory.rglob('*.dump'))
+    data_files.sort()
+    print(data_files)
 
-    # y_true shape: samples
-    y_true = d['y_true']
-    y_true_stack.append(y_true)
+    def vote(preds):
+        candidates = {k: 0 for k in [1, 2, 3, 4, 5]}
+        for i, e in enumerate(preds):
+            candidates[e] += 1
+        return sorted(candidates.items(), key=lambda e: e[1])
 
-    # y_preds shape: bands x samples
-    y_preds = [v['y_pred'] for k, v in d.items() if isinstance(k, int)]
+    data = []
+    yy_true = []
+    yy_pred = []
+    yy_pred_2 = []
+    y_probas_stack = []
+    y_true_stack = []
+    for f in data_files:
+        d = load(f)
 
-    # y_probas shape: bands x samples x classes
-    y_probas = [v['y_proba'] for k, v in d.items() if isinstance(k, int)]
-    y_probas_stack.append(y_probas)
+        # d['freqs'] = d['freqs'][:-1]
+        # d.pop(6)
 
-    for i, y_pred in enumerate(y_preds):
+        subject = d['subject_name']
+
+        # y_true shape: samples
+        y_true = d['y_true']
+        y_true_stack.append(y_true)
+
+        # y_preds shape: bands x samples
+        y_preds = [v['y_pred'] for k, v in d.items() if isinstance(k, int)]
+
+        # y_probas shape: bands x samples x classes
+        y_probas = [v['y_proba'] for k, v in d.items() if isinstance(k, int)]
+        y_probas_stack.append(y_probas)
+
+        for i, y_pred in enumerate(y_preds):
+            acc = accuracy_score(y_true=y_true, y_pred=y_pred)
+            data.append({'subject': subject, 'acc': acc, 'freqIdx': i})
+
+        y_pred = [vote(e)[-1][0] for e in np.array(y_preds).T]
+        y_pred_2 = np.argmax(np.prod(np.array(y_probas), axis=0), axis=1) + 1
+
         acc = accuracy_score(y_true=y_true, y_pred=y_pred)
-        data.append({'subject': subject, 'acc': acc, 'freqIdx': i})
+        acc_2 = accuracy_score(y_true=y_true, y_pred=y_pred_2)
+        print(acc, acc_2)
+        data.append({'subject': subject, 'acc': acc,
+                    'acc2': acc_2, 'freqIdx': 'vote'})
+        yy_true.extend(y_true)
+        yy_pred.extend(y_pred)
+        yy_pred_2.extend(y_pred_2)
 
-    y_pred = [vote(e)[-1][0] for e in np.array(y_preds).T]
-    y_pred_2 = np.argmax(np.prod(np.array(y_probas), axis=0), axis=1) + 1
+    data = pd.DataFrame(data)
+    print(data)
 
-    acc = accuracy_score(y_true=y_true, y_pred=y_pred)
-    acc_2 = accuracy_score(y_true=y_true, y_pred=y_pred_2)
-    print(acc, acc_2)
-    data.append({'subject': subject, 'acc': acc,
-                'acc2': acc_2, 'freqIdx': 'vote'})
-    yy_true.extend(y_true)
-    yy_pred.extend(y_pred)
-    yy_pred_2.extend(y_pred_2)
+    group = data.groupby(by='freqIdx')
+    print(group['acc'].mean())
+    print(group['acc2'].mean())
 
-data = pd.DataFrame(data)
-print(data)
+    yy_true = np.vstack(yy_true).ravel()
+    yy_pred = np.vstack(yy_pred).ravel()
+    yy_pred_2 = np.vstack(yy_pred_2).ravel()
 
-group = data.groupby(by='freqIdx')
-print(group['acc'].mean())
-print(group['acc2'].mean())
+    print(metrics.classification_report(y_true=yy_true, y_pred=yy_pred))
+    print(metrics.confusion_matrix(y_true=yy_true, y_pred=yy_pred))
 
-yy_true = np.vstack(yy_true).ravel()
-yy_pred = np.vstack(yy_pred).ravel()
-yy_pred_2 = np.vstack(yy_pred_2).ravel()
+    print(metrics.classification_report(y_true=yy_true, y_pred=yy_pred_2))
+    print(metrics.confusion_matrix(y_true=yy_true, y_pred=yy_pred_2))
 
-print(metrics.classification_report(y_true=yy_true, y_pred=yy_pred))
-print(metrics.confusion_matrix(y_true=yy_true, y_pred=yy_pred))
-
-print(metrics.classification_report(y_true=yy_true, y_pred=yy_pred_2))
-print(metrics.confusion_matrix(y_true=yy_true, y_pred=yy_pred_2))
-
-# %%
-y_probas_stack = np.concat(y_probas_stack, axis=1)
-print(y_probas_stack.shape)
-
-y_true_stack = np.concat(y_true_stack)
-print(y_true_stack.shape)
+    # --------------------------------------------------------------------------------
+    # Plot
+    acc = data.query('freqIdx=="vote"')['acc2'].mean()
+    x = [np.mean(f) for f in d['freqs']]
+    plt.style.use('ggplot')
+    plt.plot(x, group['acc'].mean().to_list()[:-1])
+    plt.xlabel('Freq (Hz)')
+    plt.ylabel('Acc')
+    plt.title(f'{name} | vote {acc=:0.2f}')
+    plt.hlines(y=0.2, xmin=np.min(x), xmax=np.max(
+        x), colors='gray', linestyles='-.')
+    plt.hlines(y=acc, xmin=np.min(x), xmax=np.max(
+        x), colors='blue')
+    plt.ylim([0.15, 0.5])
+    plt.tight_layout()
+    plt.savefig(output_directory.joinpath(f'{name}.png'))
+    plt.show()
 
 # %%
